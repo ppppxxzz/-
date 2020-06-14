7768be26f6ec0a9a5b60270a6fda0baf782066eb
asandy1023 <asandy1023@gmail.com>
2020-06-14 17:59:25 +0800

    a

--- js.js
+++ js.js
@@ -1,22 +1,36 @@
-$(function () {
 
 
-    var restaurant = [];
-
+/*
+var restaurant = [];
+function enterstore() {
     $.getJSON('restaurant.json', function (data) {
-      $.each(data.restaurant, function (i, f) {
-        var tblRow = '<span class="thisspan" onclick="clickspan('+ f.id +')"><div class="row">' + "<img src=" + f.picture + " class=img-thumbnail col-6>" + " </img>" +
-          "<h1 col-2>" + f.name + "</h1>" + "<p col-4>" + f.location + "</p>" + "</div></span>" + "<div><iframe src= " + f.map + " width=600 height=450 frameborder=0 style=border:0; allowfullscreen= aria-hidden=false tabindex=0></iframe></div>"
-        $(tblRow).appendTo("#restaurantData");
-      });
+        $.each(data.restaurant, function (i, f) {
+            var tblRow = '<span class="thisspan" onclick="clickspan(' + f.id + ')"><div class="row">' + "<img src=" + f.picture + " class=img-thumbnail col-6>" + " </img>" +
+                "<h1 col-2>" + f.name + "</h1>" + "<p col-4>" + f.location + "</p>" + "</div></span>" + "<div><iframe src= " + f.map + " width=600 height=450 frameborder=0 style=border:0; allowfullscreen= aria-hidden=false tabindex=0></iframe></div>"
+            $(tblRow).appendTo("#restaurantData");
+        });
 
     });
 
-  });
+}
 
-  
-         function clickspan(f){
-          window.location.href='1.html';
-          
-          }
-      
+
+*/
+
+function clickspan(f) {
+    //alert("I am an alert box!");
+    $("#restaurantData").empty();
+    var restaurant = [];
+
+    $.getJSON('restaurant.json', function (data) {
+        $.each(data.restaurant, function (i, d) {
+             if (f == d.id) {
+            var tblRow = '<span class="thisspan " onclick="clickspan(' + d.id + ')"><div class="row">' + "<img src=" + d.picture + " class=img-thumbnail col-6>" + " </img>" +
+                "<h3 col-2>" + d.name + "</h3>" + "<p col-4>" + d.location + "</p>" + "</div></span>" + "<div><iframe src= " + d.map + " width=600 height=450 frameborder=0 style=border:0; allowfullscreen= aria-hidden=false tabindex=0></iframe></div>"
+            $(tblRow).appendTo("#restaurantData");
+            }
+        });
+
+    });
+
+}
